f3cd53a231fe1b43e37eaa34980cae8ec5e4acba
Ivan5 <converse_ivan@hotmail.com>
2019-08-02 17:35:48 -0500

    Make a Navbar component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Finder"
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <h1>{this.props.title}</h1>
+      </nav>
+    );
+  }
+}
