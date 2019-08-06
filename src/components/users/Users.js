a6f3a3306c350ffc2fadb7b56485840682c9c69a
Ivan5 <converse_ivan@hotmail.com>
2019-08-06 15:29:49 -0500

    Make a Spinner component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
-export default class Users extends Component {
-  render() {
+const Users = props => {
+  if (props.loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
   gridGap: "1em"
 };
+
+export default Users;
